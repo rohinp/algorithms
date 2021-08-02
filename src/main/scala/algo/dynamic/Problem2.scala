f9e84d32cc4666154f3c133f316799405d4610b3
Rohin Patel <rohin.patel@ing.com>
2021-08-02 17:53:05 +0200

    Better solun for robot problem2

--- src/main/scala/algo/dynamic/Problem2.scala
+++ src/main/scala/algo/dynamic/Problem2.scala
@@ -8,15 +8,17 @@ The robot can only move in two directions, right and down, but certain cells are
 the robot cannot step on them. Design an algorithm to find a path for the robot from the top left to
 the bottom right.
 
+[] = []
+
 0*0
     = []
 
 1*1
-[r] = [[0,0]]
+[r] = [(0,0)]
 
 3*1     if only one column and none of the cells are offlimit then answer is [[2,0]]
 [r]     else []
-[ ]
+[-]     
 [ ]
 
 1*3
@@ -55,40 +57,6 @@ the bottom right.
 case class Point(x:Int,y:Int)
 
 //false for offlimits
-def routes2(grid:Vector[Vector[Boolean]]):List[List[Point]] =
-    def goRight(r:Int, c:Int, maxCols:Int):Option[(Int,Int)] =
-        if c < maxCols then Some((r, c + 1)) else None
-
-    def goDown(r:Int, c:Int, maxRows:Int):Option[(Int,Int)] =
-        if r < maxRows then Some((r + 1, c)) else None
-
-
-    def loop(row:Int, col:Int, lastRow:Int, lastCol:Int):List[List[Point]] = 
-        (goRight(row,col,lastCol), goDown(row,col,lastRow)) match
-            case (None, None) => List(List(Point(row,col)))
-            case (None, Some(t)) if grid(row)(col) => 
-                List(Point(row,col)) :: loop(t._1,t._2,lastRow, lastCol)
-            case (None, Some(t)) => List()
-            case (Some(t), None) if grid(row)(col) => 
-                List(Point(row,col)) :: loop(t._1,t._2,lastRow, lastCol)
-            case (Some(t), None) => List()
-            case (Some(t1), Some(t2)) => 
-                if grid(t1._1)(t1._2) && grid(t2._1)(t2._2) then 
-                    (List(Point(row,col)) :: loop(t1._1,t1._2,lastRow, lastCol)) ++
-                    (List(Point(row,col)) :: loop(t2._1,t2._2,lastRow, lastCol))
-                else if grid(t1._1)(t1._2) then List(Point(row,col)) :: loop(t1._1,t1._2,lastRow, lastCol)
-                else if grid(t2._1)(t2._2) then List(Point(row,col)) :: loop(t2._1,t2._2,lastRow, lastCol)
-                else List()
-    
-    
-    if grid.length == 0 then List() else 
-        val row = grid.length - 1
-        if grid(0).length == 0 then List() else
-            val col = grid(0).length - 1
-            if(grid(row)(col)) then
-                loop(0,0, row, col)
-            else List()
-
 def routes(grid:Vector[Vector[Boolean]]):List[Point] =
     def goRight(r:Int, c:Int, maxCols:Int):Option[(Int,Int)] =
         if c < maxCols then Some((r, c + 1)) else None
@@ -115,13 +83,39 @@ def routes(grid:Vector[Vector[Boolean]]):List[Point] =
                 else List()
     
     
-    if grid.length == 0 then List() else 
+    if grid.length == 0 ||grid(0).length == 0 then List() else 
         val row = grid.length - 1
-        if grid(0).length == 0 then List() else
-            val col = grid(0).length - 1
-            if(grid(row)(col)) then
-                loop(0,0, row, col)
-            else List()
+        val col = grid(0).length - 1
+        if(grid(row)(col)) then loop(0,0, row, col) else List()
+
+
+/*  
+
+This is a less verbose solution but the above solution is the one which gave the idea of below one.
+The idea is convert the multiple pattern match cases into a simple forexpression.
+Let the flatMap and map manage the invalid senarios i.e. taking care of all wiring.
+
+The below solution are makes sure that if multiple paths available it will give the result respectively.
+*/
+def routes2(grid:Vector[Vector[Boolean]]):List[List[Point]] =
+    if grid.length == 0 || grid(0).length == 0 then List() else 
+        val row = grid.length - 1
+        val col = grid(0).length - 1
+        val goRight:Int => Int => Option[Point] = r => c => if c < col then Some(Point(r, c + 1)) else None
+        val goDown:Int => Int => Option[Point] = r => c => if r < row then Some(Point(r + 1, c)) else None
+        if(grid(row)(col)) then 
+            def loop(row:Int, col:Int):List[List[Point]] = 
+                (goRight(row)(col), goDown(row)(col)) match
+                    case (None, None) => List(List(Point(row,col)))
+                    case (right, down) =>
+                        for 
+                            optionPoint <- List(right, down)
+                            point <- optionPoint.toList
+                            if(grid(row)(col))
+                            remaining <- loop(point.x,point.y)
+                        yield Point(row,col) :: remaining
+            loop(0,0) 
+        else List()
 
 @main def problem2 =
     println(s"${"-"*20}{ [] }${"-"*20}")
@@ -146,3 +140,24 @@ def routes(grid:Vector[Vector[Boolean]]):List[Point] =
     [-][-][ ] */
     println(s"${"-"*20}{ 3*3}${"-"*20}")
     routes(Vector(Vector(true,true,false),Vector(false,true,true),Vector(false,false,true))) tap println
+    routes2(Vector(Vector(true,true,false),Vector(false,true,true),Vector(false,false,true))) tap println
+    /* 4*4
+    [r][ ][-][ ]
+    [-][ ][ ][ ]
+    [-][-][ ][ ]
+    [-][-][ ][ ] 
+    
+    In this case there are multiple solutions
+    so the expected out put is this.
+
+    List(Point(0,0), Point(0,1), Point(1,1), Point(1,2), Point(1,3), Point(2,3), Point(3,3))
+    List(Point(0,0), Point(0,1), Point(1,1), Point(1,2), Point(2,2), Point(2,3), Point(3,3))
+    List(Point(0,0), Point(0,1), Point(1,1), Point(1,2), Point(2,2), Point(3,2), Point(3,3))
+    */
+    println(s"${"-"*20}{ 4*4}${"-"*20}")
+    routes2(Vector(
+        Vector(true,true,false,true),
+        Vector(false,true,true,true),
+        Vector(false,false,true,true),
+        Vector(false,false,true,true)
+    )).mkString("\n") tap println
---
+++ src/main/scala/algo/dynamic/Problem3.scala
@@ -0,0 +1,13 @@
+package algo.dynamic
+
+/* 
+Magic Index: A magic index in an array A [e ... n -1] is defined to be an index such that 
+  A[i] = i. 
+Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in
+array A.
+  FOLLOW UP
+  What if the values are not distinct? 
+*/
+
+@main def problem3 =
+  println("Code here...")
